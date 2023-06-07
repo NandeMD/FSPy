a752c6d983893cd3b7514c11de4c641f11c81966
NandeMD <celikumutcan2001@gmail.com>
2023-06-07 19:01:58 +0300

    added sessin list and it's response model

--- fspy/response_models.py
+++ fspy/response_models.py
@@ -1,15 +1,38 @@
 from dataclasses import dataclass
 from typing import List
 
+
 @dataclass
-class SessionsListResponse(slots=True):
+class FlareSolverNotOK:
+    status: str
+    version: str
+    message: str
+
+    @classmethod
+    def from_dict(cls, dct):
+        return FlareSolverNotOK(
+            dct["status"],
+            dct["version"],
+            dct["message"]
+        )
+
+
+@dataclass
+class SessionsListResponse:
     startTimestamp: int
     endTimestamp: int
     version: str
     status: str
     message: str
     sessions: List[str]
-    
+
     @classmethod
-    def from_dict(cls, dct: dict):
-        return SessionsListResponse(**dct)
+    def from_dict(cls, dct):
+        return SessionsListResponse(
+            dct["startTimestamp"],
+            dct["endTimestamp"],
+            dct["version"],
+            dct["status"],
+            dct["message"],
+            dct["sessions"]
+        )
--- fspy/solver.py
+++ fspy/solver.py
@@ -1,31 +1,40 @@
-from typing import Union, Literal, Optional
+from typing import Union, Literal, Optional, List
 import requests
+from .response_models import FlareSolverNotOK, SessionsListResponse
 import orjson
+
 
 class FlareSolverrError(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
 
+
 class FlareSolverr:
     def __init__(
-        self, 
-        host: str = "127.0.0.1", 
-        port: Optional[Union[str, int]] = "8191",
-        http_schema: Literal["http", "https"] = "http",
-        additional_headers: Optional[dict] = None
-        ) -> None:
+            self,
+            host: str = "127.0.0.1",
+            port: Optional[Union[str, int]] = "8191",
+            http_schema: Literal["http", "https"] = "http",
+            additional_headers: dict = {},
+            v: str = "v1"
+    ) -> None:
         self.req_session = requests.Session()
         self.req_session.headers.update = {"Content-Type": "application/json", **additional_headers}
-        
+
         self.host = host
-        self.port = str(port)
+        self.port = str(port) if port is not None else None
         self.http_schema = http_schema
-        self.flare_solverr_url = f"{http_schema}://{host}{':'+self.port if port is not None else ''}"
-        
+        self.v = v
+        self.flare_solverr_url = f"{http_schema}://{host}{':' + self.port if port is not None else ''}/{v}"
+
     @property
-    def sessions(self):
+    def sessions(self) -> Union[List[str], FlareSolverNotOK]:
         payload = {
             "cmd": "sessions.list"
         }
-        response = self.req_session.get(self.flare_solverr_url)
-        
+        response = self.req_session.post(self.flare_solverr_url, json=payload)
+        response_dict = orjson.loads(response.content)
+
+        if response_dict["status"] != "ok":
+            return FlareSolverNotOK.from_dict(response_dict)
+        return SessionsListResponse.from_dict(response_dict).sessions
